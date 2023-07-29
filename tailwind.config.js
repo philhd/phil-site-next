e6f823896a3fff82be7f69723bd5c3f71cfec9b4
Phil Hardegen <philhardegen@gmail.com>
2023-07-28 22:48:57 -0400

    stripped down and start customizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      white: 'rgb(251, 248, 241)',
+      black: 'rgb(19, 15, 23)'
+    },
+    fontFamily: {
+      mono: ['Andale mono', 'Courier new'],
+      sans: ['Helvetica', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
