b7f782eb09bc790f2dc9a686a747394f657d8a2a
Phil Hardegen <philhardegen@gmail.com>
2023-08-12 21:22:55 -0400

    switch to class based dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html className='dark' lang="en">
+      <body className='bg-white dark:bg-black text-black dark:text-white'>{children}</body>
     </html>
   )
 }
