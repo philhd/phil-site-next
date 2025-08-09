54b7f1e8a300dbd77078e2805008694034ffc66c
Phil Hardegen <philhardegen@gmail.com>
2025-08-09 16:50:01 -0400

    initial next upgrades

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Phil Hardegen\'s website',
-  description: 'A place to show some of the things that I am doing. It\'s focused on computer programming, and other things I\'m interested in.',
+  description: 'A place to show some of the things that I am doing. It\'s focused on computer programming, and other things I\'m interested in.'
 }
 
 export default function RootLayout({
