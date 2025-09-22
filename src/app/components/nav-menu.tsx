290221ca590a92cfb02c92b7143ea07976c00731
Phil Hardegen <philhardegen@gmail.com>
2025-09-21 23:47:38 -0400

    sticky top menu

--- src/app/components/nav-menu.tsx
+++ src/app/components/nav-menu.tsx
@@ -1,7 +1,9 @@
 export default function NavMenu({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex items-center py-4 space-x-5 text-2xl">
-            { children }
+        <div className="sticky top-0 bg-white dark:bg-black z-50 border-b border-gray-200 dark:border-gray-700">
+            <div className="flex items-center py-4 space-x-5 text-2xl">
+                { children }
+            </div>
         </div>
     )
 }
