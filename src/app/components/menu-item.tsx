281982b235801d4b73a845c4060650e6667b7b91
Phil Hardegen <philhardegen@gmail.com>
2025-09-21 23:02:42 -0400

    mobilize home menu item

--- src/app/components/menu-item.tsx
+++ src/app/components/menu-item.tsx
@@ -1,7 +1,22 @@
 import Link from "next/link";
 
 export default function MenuItem({ name, href }: { name: string, href: string }) {
+    const isHome = name === 'Home'
+
+    if (isHome) {
+        return (
+            <Link className="text-black dark:text-white text-2xl hover:font-black no-underline" href={href}>
+                <span className="block sm:hidden">
+                    <i className="fa-solid fa-house"></i>
+                </span>
+                <span className="hidden sm:block">{name}</span>
+            </Link>
+        )
+    }
+
     return (
-        <Link className="text-black dark:text-white text-2xl hover:font-black no-underline" href={href}>{name}</Link>
+        <Link className="text-black dark:text-white text-2xl hover:font-black no-underline" href={href}>
+            {name}
+        </Link>
     )
 }
