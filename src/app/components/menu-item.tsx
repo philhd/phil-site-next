1d1d5a43a85c033e1e7684b4aeb7488985043129
Phil Hardegen <philhardegen@gmail.com>
2025-09-21 23:29:10 -0400

    menu item upgrades

--- src/app/components/menu-item.tsx
+++ src/app/components/menu-item.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
 
 export default function MenuItem({ name, href }: { name: string, href: string }) {
-    const isHome = name === 'Home'
-
-    if (isHome) {
-        return (
-            <Link className="text-black dark:text-white text-2xl hover:font-black no-underline" href={href}>
-                <span className="block sm:hidden">
-                    <i className="fa-solid fa-house"></i>
-                </span>
-                <span className="hidden sm:block">{name}</span>
-            </Link>
-        )
+    const getIcon = (menuName: string) => {
+        switch (menuName) {
+            case 'Home':
+                return 'fa-solid fa-house'
+            case 'Blog':
+                return 'fa-solid fa-memo'
+            case 'About':
+                return 'fa-solid fa-circle-info'
+            default:
+                return 'fa-solid fa-circle'
+        }
     }
 
     return (
         <Link className="text-black dark:text-white text-2xl hover:font-black no-underline" href={href}>
-            {name}
+            <span className="block sm:hidden">
+                <i className={getIcon(name)}></i>
+            </span>
+            <span className="hidden sm:block">{name}</span>
         </Link>
     )
 }
