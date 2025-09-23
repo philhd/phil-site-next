46f25c7cd7b70a96103799a76f0f3f7822c8e606
Phil Hardegen <philhardegen@gmail.com>
2025-09-22 21:51:10 -0400

    breathing room

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <main>
+    <main className="pt-8">
       <p>
         Hi. My name is Phil Hardegen. I live in the Northeastern United States and I do software engineering. I&apos;m also into music and art. The reason this site is here is to share some of the things that I&apos;ve done. I hope you get some enjoyment out of it.
       </p>
