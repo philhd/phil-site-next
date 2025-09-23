a3eb1243ad503667d775e25c9a8f8317591c6af5
Phil Hardegen <philhardegen@gmail.com>
2025-09-22 21:59:05 -0400

    go back to left justify top nav on mobile

--- src/app/components/nav-menu.tsx
+++ src/app/components/nav-menu.tsx
@@ -1,7 +1,7 @@
 export default function NavMenu({ children }: { children: React.ReactNode }) {
     return (
         <div className="sticky top-0 bg-white dark:bg-black z-50 border-b border-gray-200 dark:border-gray-700">
-            <div className="flex items-center py-4 text-2xl justify-evenly sm:justify-start sm:space-x-5">
+            <div className="flex items-center py-4 space-x-5 text-2xl">
                 { children }
             </div>
         </div>
